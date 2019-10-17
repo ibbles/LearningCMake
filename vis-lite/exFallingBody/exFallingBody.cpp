6f83cde3faeabd37067fe2d05759ba6b2a17c33b
Martin Nilsson <ibbles@hotmail.com>
2019-10-17 22:23:25 +0200

    Use RigidBody in exFallingBody

--- vis-lite/exFallingBody/exFallingBody.cpp
+++ vis-lite/exFallingBody/exFallingBody.cpp
@@ -1,24 +1,10 @@
-#include "config.h"
+#include <visPhysics/visPhysics.hpp>
 
 #include <iostream>
 
-int main(int argc, char* argv[])
+int main(int /*argc*/, char* /*argv*/[])
 {
-    if (argc < 2)
-    {
-        std::cout
-            << argv[0] << " Version "
-            << exFallingBody_VERSION_MAJOR << '.' << exFallingBody_VERSION_MINOR
-            << '\n';
-        #if MY_VERBOSE
-        std::cout
-            << "Major version is " << exFallingBody_VERSION_MAJOR << '\n'
-            << "Minor version is " << exFallingBody_VERSION_MINOR << '\n';
-        #endif
-        return 1;
-    }
-
-    double value = std::stod(argv[1]);
-    std::cout << "The value is "<< value << '\n';
+    visPhysics::RigidBody my_body;
+    std::cout << my_body.getPosition().x << '\n';
     return 0;
 }
