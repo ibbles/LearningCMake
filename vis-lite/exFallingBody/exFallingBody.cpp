09f44304a2e5a025a63de08eeb7de4a4a8534e20
Martin Nilsson <ibbles@hotmail.com>
2019-11-03 13:39:08 +0100

    Implement exFallingBoxy

--- vis-lite/exFallingBody/exFallingBody.cpp
+++ vis-lite/exFallingBody/exFallingBody.cpp
@@ -4,7 +4,24 @@
 
 int main(int /*argc*/, char* /*argv*/[])
 {
-    visPhysics::RigidBody my_body;
-    std::cout << my_body.getPosition().x << '\n';
+    // Simulation settings.
+    float const timestep {1.0F / 60.0F};
+    float const end_time {10.0F};
+
+    // World settings.
+    visMath::Vector gravity {0.0F, 0.0F, -9.82F};
+
+    // Scene setup.
+    visPhysics::RigidBody body;
+
+    // Main loop.
+    float time {0.0F};
+    while (time < end_time)
+    {
+        time += timestep;
+        visPhysics::applyGravity(body, gravity);
+        visPhysics::integrate(body, timestep);
+        std::cout << body.position.z << '\n';
+    }
     return 0;
 }
